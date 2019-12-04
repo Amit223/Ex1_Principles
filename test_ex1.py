0c1f3a5df2d4042efe6dbc52c07a3fd7df3a57d8
ofirgru <ofirgru@post.bgu.ac.il>
2019-12-04 12:36:08 +0200

    fixed rept + 1.10

--- test_ex1.py
+++ test_ex1.py
@@ -1,24 +1,31 @@
 from Ex1 import ComplexNum
 
 x=ComplexNum(1, 2)
-print(x.re)
-print(x.im)
-print(x.to_tuple())
-print(str(x))
+# print(x.re)
+# print(x.im)
+# print(x.to_tuple())
+# print(str(x))
 
 y1=ComplexNum(2,2)
 y2=ComplexNum(1,3)
 y3=ComplexNum(1,2)
 
-print("add check")
-print(y1+x)
-print(y2+x)
-print(y3+x)
-
-print("neg check:")
-print(x.__neg__())
-
-print("- check:")
-print(y1-x)
-print(y2-x)
-print(y3-x)
+# print("add check")
+# print(y1+x)
+# print(y2+x)
+# print(y3+x)
+#
+# print("neg check:")
+# print(x.__neg__())
+#
+# print("- check:")
+# print(y1-x)
+# print(y2-x)
+# print(y1*(y1.conjugate()))
+# print(ComplexNum(0,1))
+# print(ComplexNum(1,0))
+# print(ComplexNum(-1,1))
+# print(ComplexNum(0,0))
+# print(ComplexNum(-1,-1))
+# print(ComplexNum(-1,0))
+# print(ComplexNum(0,-1))
